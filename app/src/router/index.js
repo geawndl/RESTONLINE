0ba36d32c880a675ede22445720c99f228dc50e2
ZERO <zero20022003@gmail.com>
2022-10-24 11:42:06 +0200

    création login

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,7 +4,23 @@ const routes = [
     {
         name: 'Home',
         path : '/',
-        component: () => import("@/pages/HomePage")
+        component: () => import("@/views/Home")
+    },    {
+        name: 'Login',
+        path : '/login',
+        component: () => import("@/views/Login")
+    },    {
+        name: 'Register',
+        path : '/register',
+        component: () => import("@/views/Register")
+    },    {
+        name: 'DashBoard',
+        path : '/dashboard',
+        component: () => import("@/views/DashBoard")
+    },    {
+        name: 'Commande',
+        path : '/commande',
+        component: () => import("@/views/Commande")
     },
 ]
 export const router = createRouter({
